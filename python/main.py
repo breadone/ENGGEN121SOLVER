612ff750823004c30d073a9e9901016fd714be9c
pradyun <pradyun.setti@gmail.com>
2021-03-08 13:22:42 +1300

    tried to do python version but bruh i hate python

---
+++ python/main.py
@@ -0,0 +1,10 @@
+from Statics import *
+
+
+print("having trouble with an ENGGEN 121 problem? so was i. select which type of problem youre having from:")
+print("1. Moments")
+
+choice = input("choice: ")
+
+if choice == 1:
+    momentSolver()
