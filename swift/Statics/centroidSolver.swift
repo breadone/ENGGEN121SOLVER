f690f851959d5d81b7a98b6410876bf78fcc20a2
pradyun <pradyun.setti@gmail.com>
2021-03-12 11:09:52 +1300

    S: input stage done for centroid solver, on to logic stage now

--- swift/Statics/centroidSolver.swift
+++ swift/Statics/centroidSolver.swift
@@ -7,8 +7,37 @@
 
 import Foundation
 
-func centroidSolver() {
+struct Shape {
+    let area: Double
+    let xDist, yDist: Double
     
+}
+
+func centroidSolver() {
+    print("how many subshapes: ")
+    let subShapeCount: Int = Int(readLine()!)!
+    
+    var shapeArray = [Shape]()
+    
+    print("enter area of main shape: ")
+    let mainShapeArea = Double(readLine() ?? "0")!
+    print("enter x distance from reference point to centre of shape")
+    let mainShapeXDist = Double(readLine() ?? "0")!
+    print("enter y distance from reference point to centre of shape")
+    let mainShapeYDist = Double(readLine() ?? "0")!
+    
+    var mainShape = Shape(area: mainShapeArea, xDist: mainShapeXDist, yDist: mainShapeYDist)
+    
+    for _ in 0...subShapeCount {
+        print("enter area of main shape: ")
+        let newShapeArea = Double(readLine() ?? "0")!
+        print("enter x distance from reference point to centre of shape")
+        let newShapeXDist = Double(readLine() ?? "0")!
+        print("enter y distance from reference point to centre of shape")
+        let newShapeYDist = Double(readLine() ?? "0")!
+        
+        shapeArray.append(Shape(area: newShapeArea, xDist: newShapeXDist, yDist: newShapeYDist))
+    }
     
     
 }
